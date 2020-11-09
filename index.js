2e875ce3ad45ee8b08d6759a4780c629937f1bfb
FaheemQuazi <faheemquazi@gmail.com>
2020-11-09 07:20:13 +0000

    Integrate Auth0 (pls no dox)

--- index.js
+++ index.js
@@ -1,16 +1,43 @@
 // Owner: Faheem Quazi
 
-var express = require('express');
-var session = require('express-session');
-var path = require('path');
+const express = require('express');
+const session = require('express-session');
+const path = require('path');
+const passport = require('passport');
+const Auth0Strategy = require('passport-auth0');
+const jsonwebtoken = require('jsonwebtoken');
 
 var app = express();
 
 /* Configure ExpressJS */
-app.use(session({secret: "itsnotreallyasecretifitsopensource"})); // Session Management
 app.use(express.static('./public')); // Static CSS/JS/image resources that should always be accessible
 app.set('view engine', 'ejs'); // Set the template engine
 app.set('views', path.join(__dirname, 'views')); // Set the path to grab views
+app.use(session({secret: "itsnotreallyasecretifitsopensource"})); // Session Management
+
+/* Configure Auth0 */
+var strategy = new Auth0Strategy(
+  {
+    domain: "cosc4351test.us.auth0.com",
+    clientID: "Un2Bbm95YSh9Pn36ISwf6ll1er2p8aH1",
+    clientSecret: "doIQHnhqlTTO9OrrzPgDfL4afX3GOKgO8Ov2z4Da6ZvltWNz3E-2Em_swg6KNZgx",
+    callbackURL: 'https://3000-e21fd409-0c6b-4747-bd22-5e16bdb8afff.ws-us02.gitpod.io/auth/callback'
+  },
+  function (accessToken, refreshToken, extraParams, profile, done) {
+    var decoded = jsonwebtoken.decode(extraParams.id_token);
+    profile.permissions = decoded['https://any-namespace/roles'];
+    return done(null, profile);
+  }
+);
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(strategy);
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
 
 /* App Routes */
 app.use('/auth', require('./routes/auth'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,30 +1,39 @@
 // Owner: Faheem Quazi
 
 const routes = require('express').Router();
+const passport = require('passport');
+const querystring = require('querystring');
 
-routes.get('/login', (req, res) => {
-    if (req.session['loggedIn'] == true) {
-        res.redirect('/auth/success');
-    } else { 
-        // TODO: Add SSO middleware
-        res.send('login');
-    }
+routes.get('/login', passport.authenticate('auth0', {
+  scope: 'openid email profile'
+}), function (req, res) {
+  res.redirect('/');
 });
 
-routes.get('/success', (req, res) => {
-    // TODO: Redirect to portal home or admin based on user type
-    req.session['loggedIn'] = true;
-    res.send('post login (login successful)');
-});
-
-routes.get('/err', (req, res) => {
-    req.session['loggedIn'] = false;
-    res.send('post login (login failed)');
+routes.get('/callback', (req, res, next) => {
+    passport.authenticate('auth0', function (err, user, info) {
+        if (err) { return next(err); }
+        if (!user) { return res.redirect('/auth/login'); }
+        req.logIn(user, function (err) {
+            console.log(user);
+            res.redirect('/portal/home');
+        });
+    })(req, res, next);
 });
 
 routes.get('/logout', (req, res) => {
-    req.session['loggedIn'] = false;
-    res.redirect('/');
+    req.logout();
+
+    var returnTo = "https://3000-e21fd409-0c6b-4747-bd22-5e16bdb8afff.ws-us02.gitpod.io"
+    var logoutURL = new URL(
+        "https://cosc4351test.us.auth0.com/v2/logout"
+    );
+    logoutURL.search = querystring.stringify({
+        client_id: "Un2Bbm95YSh9Pn36ISwf6ll1er2p8aH1",
+        returnTo: returnTo
+    });
+
+    res.redirect(logoutURL);
 });
 
 module.exports = routes;
