facdcc7089fedf26078ebb833ab849dac5f44bc3
FaheemQuazi <faheemquazi@gmail.com>
2020-11-12 04:31:55 +0000

    change appURL to non-const, since we're modifying it now

--- appURL.js
+++ appURL.js
@@ -5,7 +5,7 @@ module.exports = () => {
     try {
         gp = execSync("gp url 3000").toString().slice(0,-1);
     } catch (error) {}
-    const appURL = (gp || process.env["VERCEL_URL"].split('/')[0] || process.env["COSC_CALLBACK_URL"]);
+    var appURL = (gp || process.env["VERCEL_URL"].split('/')[0] || process.env["COSC_CALLBACK_URL"]);
     if (!(appURL.startsWith('http://') || appURL.startsWith('https://'))) {
         appUrl = 'https://' + appURL;
     }
