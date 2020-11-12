9e902040cd5b02b27f0c2aed197934caab57e0d2
FaheemQuazi <faheemquazi@gmail.com>
2020-11-12 04:38:26 +0000

    More appURL Logic Fixes
    - Improve Readability
    - Reorder vars to prioritize COSC_CALLBACK_URL
    - Remove VERCEL_URL string mods

--- appURL.js
+++ appURL.js
@@ -2,12 +2,15 @@ const { execSync } = require("child_process");
 
 module.exports = () => {
     var gp = undefined;
+
     try {
         gp = execSync("gp url 3000").toString().slice(0,-1);
     } catch (error) {}
-    var appURL = (gp || process.env["VERCEL_URL"].split('/')[0] || process.env["COSC_CALLBACK_URL"]);
-    if (!(appURL.startsWith('http://') || appURL.startsWith('https://'))) {
-        appUrl = 'https://' + appURL;
+
+    var appURL = (gp || process.env["COSC_CALLBACK_URL"] || process.env["VERCEL_URL"] );
+    
+    if (!(appURL.startsWith("https://") || appURL.startsWith("http://"))) {
+        appURL = "https://" + appURL;
     }
     return appURL;
 }
